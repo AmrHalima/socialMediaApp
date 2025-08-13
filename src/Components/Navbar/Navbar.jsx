2e63e4ab27616358a167efa8f19de96cea3e30b0
AmrHalima <amr158halima@icloud.com>
2025-08-13 18:11:44 +0300

    feat: Update favicon and logo for consistency in branding

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -14,9 +14,9 @@ function Navbar() {
                     className="flex items-center space-x-3 rtl:space-x-reverse"
                 >
                     <img
-                        src="https://flowbite.com/docs/images/logo.svg"
+                        src="/logo.png"
                         className="h-8"
-                        alt="Flowbite Logo"
+                        alt="Logo"
                     />
                     <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
                         Social Media App
